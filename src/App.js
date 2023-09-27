e5b91fccbb20c0cbb8af0e383c06dfbd2c7adeca
Antkilla <132300802+Antkilla@users.noreply.github.com>
2023-09-26 18:37:21 -0700

    video-react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/> 
+    </div>
+  )
+}
+export default App;
