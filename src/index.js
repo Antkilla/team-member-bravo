a4dfe937bf92bb629102e9f0096a34577809649f
Antkilla <132300802+Antkilla@users.noreply.github.com>
2023-09-26 20:07:55 -0700

    app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { ceateRoot } from 'react-dom/client';
 import App from './App';
+import 'bootstrap/dist/CSS/bootstrap.min.CSS';
+const container = document.getElementById("root");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(container)
+
 root.render(
   <React.StrictMode>
     <App />
