0c40d07a3cde85174162a2d048018708e79a45df
Antkilla <132300802+Antkilla@users.noreply.github.com>
2023-09-28 17:29:27 -0700

    css

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -91,24 +91,28 @@ const Employees = () => {
   }]);
 
 
-    return (
-        <main className="container">
-            <div class="row">
-                <div class ="col-8">   
-                {employees.map((employee)=> (
-                    <div id={employee.id} className="card">
-                        <img src={femaleProfileImage} className="card-img-top" />   
-                        <div className="card-body">
-                            <h5 className="card-title">Full Name: {employee.fullName}</h5>
-                            <p className="card-text"><b>Designation:</b> {employee.designation}</p>
-                        </div>
-                    </div>
-                 ))   
-                }
-                 </div>            
-            </div> 
-        </main>
-    );    
-
+  return (
+    <main className="container">
+      <div className="row justify-content-center mt-3 mb-3">
+        <div className="col-8">
+          <div class="card-collection">
+            {employees.map((employee) => (
+              <div id={employee.id} className="card m-2" style= {{ cursor: "pointer" }}>
+                
+                {(employee.gender === 'male') ? <img src={maleProfileImage} className="card-img-top" />   
+                   : <img src={femaleProfileImage} className="card-img-top" />} 
+                
+                <div className="card-body">
+                  <h5 className="card-title">Full Name: {employee.fullName}</h5>
+                  <p className="card-text"><b>Designation:</b> {employee.designation}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div> 
+    </main>
+  );
+  
 };
 export default Employees;
